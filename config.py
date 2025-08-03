5c76aaecb37f05cf81e4a2f1f3e0c435986277ef
PINKI AKTER <74209121+pinkfloyed@users.noreply.github.com>
2025-08-03 23:35:55 +0600

    feat : Configured Base Paths and Directories
    - Added base directory configuration for dataset, user uploads, and history
    - For ChromaDB added directory

--- config.py
+++ config.py
@@ -1,9 +1,8 @@
 from pathlib import Path
 
-# Base directory (project root)
 BASE_DIR = Path(__file__).parent.absolute()
 
-# Path configurations
+
 DATASET_DIR = BASE_DIR / "data" / "dataset"
 UPLOAD_DIR = BASE_DIR / "static" / "uploads"
 HISTORY_DIR = BASE_DIR / "static" / "history"
@@ -14,3 +13,4 @@ print(f"DATASET_DIR: {DATASET_DIR}")
 print(f"UPLOAD_DIR: {UPLOAD_DIR}")
 print(f"HISTORY_DIR: {HISTORY_DIR}")
 print(f"CHROMA_DB_DIR: {CHROMA_DB_DIR}")
+
