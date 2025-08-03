01a5b5281d6f09cf9bdfa63c8fc3112edbcedafc
PINKI AKTER <74209121+pinkfloyed@users.noreply.github.com>
2025-08-03 23:13:23 +0600

    feat: Initialize FastAPI app with environment setup, models, and routers
    - Added routers for gallery, upload, view, chat, and search functionalities
    - Configured application to run locally on port 8000

--- main.py
+++ main.py
@@ -10,26 +10,18 @@ from backend.chroma_service import get_collection
 from backend.routes import gallery, upload, view, chat, search
 from config import UPLOAD_DIR, HISTORY_DIR
 
-# Load environment variables
 load_dotenv()
 
-# Initialize API keys and models
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=GOOGLE_API_KEY)
 
-# Initialize CLIP model
 device = "cuda" if torch.cuda.is_available() else "cpu"
 clip_model, preprocess = clip.load("ViT-B/32", device=device)
 
-# Initialize YOLO model
-# yolo_model = torch.hub.load('ultralytics/yolov5', 'yolov5x', pretrained=True)
-
 collection = get_collection()
 
-# Create FastAPI app
 app = FastAPI()
 
-# Added CORS middleware
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -38,16 +30,16 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# To ensure directories exist
+
 UPLOAD_DIR.mkdir(parents=True, exist_ok=True)
 HISTORY_DIR.mkdir(parents=True, exist_ok=True)
 
-# Set up templates and static files
+
 templates = Jinja2Templates(directory="templates")
 app.mount("/static_files", StaticFiles(directory="templates/static_files"), name="static_files")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# Include routers
+
 app.include_router(gallery.router)
 app.include_router(upload.router)
 app.include_router(view.router)
@@ -56,3 +48,4 @@ app.include_router(search.router)
 
 if __name__ == '__main__':
     uvicorn.run("main:app", host="localhost", port=8000, reload=True)
+
